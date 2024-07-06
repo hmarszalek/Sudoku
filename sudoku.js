3be721083bc682ab57a16ca734cdc7bc0ec12bc7
Andromache2902 <andromache@Andromache.localdomain>
2024-07-06 16:28:02 +0200

    Remove example board

--- sudoku.js
+++ sudoku.js
@@ -4,30 +4,6 @@ var tileSelected = null;
 var BOARD_SIZE = 9;
 var BOX_SIZE = 3;
 
-var board = [
-    "--74916-5",
-    "2---6-3-9",
-    "-----7-1-",
-    "-586----4",
-    "--3----9-",
-    "--62--187",
-    "9-4-7---2",
-    "67-83----",
-    "81--45---"
-]
-
-var solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
-
 window.onload = function() {
     setGame();
 }
@@ -37,7 +13,7 @@ function setGame() {
     let sudoku = new Sudoku(40)
     solution = sudoku.solution;
     board = sudoku.board;
-    
+
     // Digits
     for (let i = 1; i <= 9; i++) {
         let number = document.createElement("div");
