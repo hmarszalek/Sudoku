11b2678b1d31333f07c5ceaf46c4006b15340a97
Andromache2902 <andromache@Andromache.localdomain>
2024-07-06 16:16:39 +0200

    Apply basic game logic - select number and tile

--- sudoku.js
+++ sudoku.js
@@ -38,6 +38,7 @@ function setGame() {
         let number = document.createElement("div");
         number.id = i
         number.innerText = i;
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
@@ -59,8 +60,38 @@ function setGame() {
                 tile.classList.add("vertical-line");
             }
 
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").appendChild(tile);
+        }
+    }
+}
+
+function selectNumber() {
+    if(numSelected != null) {
+        numSelected.classList.remove("number-selected");
+        if(numSelected == this) {
+            numSelected = null;
+            return;
+        }
+    }
+
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if(numSelected) {
+        if(this.innerText != "") {
+            return;
+        }
+
+        let coords = this.id.split(":");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if(solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id
         }
     }
 }
