4b705ac525dc6fc81b0c1b2cd61e5f73b55d0334
Andromache2902 <andromache@Andromache.localdomain>
2024-07-06 16:24:34 +0200

    Add sudoku class that generates random boards

--- sudoku.js
+++ sudoku.js
@@ -33,6 +33,11 @@ window.onload = function() {
 }
 
 function setGame() {
+    // Generate board
+    let sudoku = new Sudoku(40)
+    solution = sudoku.solution;
+    board = sudoku.board;
+    
     // Digits
     for (let i = 1; i <= 9; i++) {
         let number = document.createElement("div");
@@ -93,5 +98,138 @@ function selectTile() {
         if(solution[r][c] == numSelected.id) {
             this.innerText = numSelected.id
         }
+    }
+}
+
+
+class Sudoku {
+    constructor(removedDigits) {
+        this.removedDigits = removedDigits;
+        this.board = Array.from({ length: BOARD_SIZE }, () => new Array(BOARD_SIZE).fill(0))
+        this.solution = Array(BOARD_SIZE);
+
+        this.fillSudoku();
+        for(let i = 0; i < BOARD_SIZE; i++) {
+            this.solution[i] = (this.board[i]).join('');
+        }
+        this.removeDigits();
+    }
+
+    fillSudoku() {
+        this.fillDiagonalBoxes();
+        this.fillRemainingBoxes(0, BOX_SIZE);
+    }
+
+    fillDiagonalBoxes() {
+        // For each box on a diagonal
+        for(let rc = 0; rc < BOARD_SIZE; rc += BOX_SIZE) {
+            // Fill it randomly
+            this.fillBox(rc, rc);
+        }
+    }
+
+    fillRemainingBoxes(row, col) {
+        // If we are at the end of board - return
+        if (row === BOARD_SIZE - 1 && col === BOARD_SIZE) {
+            return true;
+        }
+    
+        // When reached the end of row move to the next one
+        if (col === BOARD_SIZE) {
+            row += 1;
+            col = 0;
+        }
+    
+        // Skip cells that are already filled
+        if (this.board[row][col] !== 0) {
+            return this.fillRemainingBoxes(row, col + 1);
+        }
+    
+        // Try filling the current cell with a valid value
+        for (let num = 1; num <= BOARD_SIZE; num++) {
+            if (this.isValidNumber(row, col, num)) {
+                this.board[row][col] = num;
+                if (this.fillRemainingBoxes(row, col + 1)) {
+                    return true;
+                }
+                this.board[row][col] = 0;
+            }
+        }
+    
+        // No valid value was found, so backtrack
+        return false;
+    }
+
+    fillBox(row, col) {
+        let num = 0;
+        for (let i = 0; i < BOX_SIZE; i++) {
+            for (let j = 0; j < BOX_SIZE; j++) {
+                while (true) {
+                    num = Math.floor(Math.random() * BOARD_SIZE + 1);
+                    if (!this.isUsedInBox(row, col, num)) {
+                        break;
+                    }
+                }
+                this.board[row + i][col + j] = num;
+            }
+        }
+    }
+
+    // Check if given number is used in box [row,col]x[row+3,col+3]
+    isUsedInBox(row, col, num) {
+        for (let i = 0; i < BOX_SIZE; i++) {
+            for (let j = 0; j < BOX_SIZE; j++) {
+                if (this.board[row + i][col + j] === num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Check if given number is used in given row
+    isUsedInRow(row, num) {
+        for (let col = 0; col < BOARD_SIZE; col++) {
+            if (this.board[row][col] === num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check if given number is used in given column
+    isUsedInCol(col, num) {
+        for (let row = 0; row < BOARD_SIZE; row++) {
+            if (this.board[row][col] === num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check if safe to put in cell
+    isValidNumber(i, j, num) {
+        return (
+            !this.isUsedInRow(i, num) &&
+            !this.isUsedInCol(j, num) &&
+            !this.isUsedInBox(i - (i % BOX_SIZE), j - (j % BOX_SIZE), num)
+        );
+    }
+
+    // Remove given nomber of digits
+    removeDigits() {
+        let count = this.removedDigits;
+
+        while (count !== 0) {
+            // extract coordinates i and j
+            let row = Math.floor(Math.random() * BOARD_SIZE);
+            let col = Math.floor(Math.random() * BOARD_SIZE);
+            if (this.board[row][col] !== '-') {
+                this.board[row][col] = '-';
+                count--;
+            }
+        }
+
+        return;
     }
 }
